83063d7569c097f0034029a0c2fb9341f9d1acfc
Harshad Shewale <uselesslap123@gmail.com>
2025-12-14 05:34:14 +0000

    make it little visible

--- src/components/landing/summary.tsx
+++ src/components/landing/summary.tsx
@@ -11,7 +11,7 @@ export function Summary() {
         <motion.div
             initial={{ opacity: 0, y: 50 }}
             whileInView={{ opacity: 1, y: 0 }}
-            viewport={{ once: true, amount: 0.5 }}
+            viewport={{ once: true, amount: 0.2 }}
             transition={{ duration: 0.5 }}
         >
             <SectionTitle>About Me</SectionTitle>
